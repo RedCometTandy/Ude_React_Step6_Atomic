da3ec464d9c52df9466e4014aea24fa12f03c150
RedCometTandy <85377159+RedCometTandy@users.noreply.github.com>
2022-08-11 12:43:56 +0900

    css

--- src/components/molecules/user/UserIconWithName.tsx
+++ src/components/molecules/user/UserIconWithName.tsx
@@ -4,8 +4,8 @@ export const UserIconWithName = (props) => {
   const { image, name } = props;
   return (
     <SContainer>
-      <img height={160} width={160} src={image} alt={name} />
-      <p>{name}</p>
+      <SImg height={160} width={160} src={image} alt={name} />
+      <SName>{name}</SName>
     </SContainer>
   );
 };
@@ -13,3 +13,12 @@ export const UserIconWithName = (props) => {
 const SContainer = styled.div`
   text-align: center;
 `;
+const SImg = styled.img`
+  border-radius: 50%;
+`;
+const SName = styled.p`
+  font-size: 18px;
+  font-weight: bold;
+  margin: 0;
+  color: #40514e;
+`
