8b481c7cccbe3079e12b5bd91c0ebad500962c1f
RedCometTandy <85377159+RedCometTandy@users.noreply.github.com>
2022-08-13 14:50:31 +0900

    global state を追加

--- src/components/atoms/button/SecondaryButton.tsx
+++ src/components/atoms/button/SecondaryButton.tsx
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 import { BaseButton } from "./BaseButton";
 
-export const SecondaryButton = (props: string) => {
-  const { children } = props;
-  return <SButton>{children}</SButton>;
+export const SecondaryButton = (props) => {
+  const { children, onClick } = props;
+  return <SButton onClick={onClick}>{children}</SButton>;
 };
 
 const SButton = styled(BaseButton)`
